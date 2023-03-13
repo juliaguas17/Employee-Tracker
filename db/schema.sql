6933217933aad2113fa577352b4c1ca47685a846
Julianne <juliaguas17@gmail.com>
2023-03-13 18:03:00 -0500

    added IDENTITY(1,1)

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL IDENTITY(1,1),
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL IDENTITY(1,1),
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL IDENTITY(1,1),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
