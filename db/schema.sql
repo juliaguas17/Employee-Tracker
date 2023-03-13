a99dbcab94545b4ef9baf224316ceb18becdbf42
Julianne <juliaguas17@gmail.com>
2023-03-13 18:07:34 -0500

    edited syntax in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE department (
-    id INT NOT NULL IDENTITY(1,1),
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL IDENTITY(1,1),
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INTEGER
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL IDENTITY(1,1),
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
+    role_id INTEGER,
+    manager_id INTEGER
 );
